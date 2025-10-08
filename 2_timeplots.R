ee5e812188a8b249cd5980926708f9054b8066f7
jillwilkins <jillian.dickens@emory.edu>
2025-10-08 13:43:03 -0400

    fixed the number of states axis, added crr, uc care plots

--- 2_timeplots.R
+++ 2_timeplots.R
@@ -1,7 +1,7 @@
 # Meta --------------------------------------------------------------------
 ## Author:        Jill Wilkins
 ## Date Created:  10/2/2025
-## Date Edited:   10/7/2025
+## Date Edited:   10/8/2025
 ## Goal:          Figures that show relationship of tax adoption and outcome variable        
 ##  
 
@@ -90,52 +90,101 @@ plot2 <- hospdata %>%
     totaltax = first(totaltax[!is.na(totaltax)]),
     .groups = "drop"
   )
-View(plot2)
-scale2 <- max(plot2$totaltax) / max(plot2$avg_mcaid_prop)
+
+scale_factor <- max(plot2$avg_mcaid_prop, na.rm = TRUE) / max(plot2$totaltax, na.rm = TRUE)
 
 mcaidpropplot <- ggplot(plot2, aes(x = year)) +
-  geom_line(aes(y = avg_mcaid_prop), color = "blue", size = 1.2) +
-  geom_point(aes(y = avg_mcaid_prop), color = "blue", size = 2) +
-  geom_line(aes(y = totaltax / scale2), color = "red", size = 1.2) +
-  geom_point(aes(y = totaltax / scale2), color = "red", size = 2) +
+  geom_line(aes(y = avg_mcaid_prop), color = "blue", size = 1) +
+  geom_line(aes(y = totaltax * scale_factor), color = "red", size = 1) +
   scale_y_continuous(
-    name = "Average Medicaid Charges Proportion",
-    sec.axis = sec_axis(~ . / scale2, name = "Cumulative Provider Taxes")
+    name = "Average Medicaid Proportion",
+    sec.axis = sec_axis(~ . / scale_factor, name = "Number of States Adopted Tax")
   ) +
-  labs(
-    x = "Year",
-    title = "Average Medicaid Charges Proportion and Cumulative Provider Taxes Over Time"
-  ) +
-  theme_minimal()
+  theme_minimal() +
+  theme(
+    axis.title.y.left = element_text(color = "blue"),
+    axis.title.y.right = element_text(color = "red")
+  )
 
 ggsave("sumplots/mcaidpropplot.png", plot = mcaidpropplot, width = 8, height = 8, dpi = 300)
+
 
 # dual axis: Mcaid Discharges prop and total states with tax
 plot3 <- hospdata %>%
   filter(!is.na(mcaid_prop_discharges) & mcaid_prop_discharges < 1 & year < 2025 & year > 2003) %>%
   group_by(year) %>%
   summarise(
-    avg_mcaid_prop = mean(mcaid_prop_discharges, na.rm = TRUE),
+    avg_mcaid_dis = mean(mcaid_prop_discharges, na.rm = TRUE),
     totaltax = first(totaltax[!is.na(totaltax)]),
     .groups = "drop"
   )
 
-scale3 <- max(plot3$totaltax) / max(plot3$avg_mcaid_prop)
+scale3 <- max(plot3$avg_mcaid_dis, na.rm = TRUE) / max(plot3$totaltax, na.rm = TRUE)
 
 mcaiddispropplot <- ggplot(plot3, aes(x = year)) +
-  geom_line(aes(y = avg_mcaid_prop), color = "blue", size = 1.2) +
-  geom_point(aes(y = avg_mcaid_prop), color = "blue", size = 2) +
-  geom_line(aes(y = totaltax / scale3), color = "red", size = 1.2) +
-  geom_point(aes(y = totaltax / scale3), color = "red", size = 2) +
+  geom_line(aes(y = avg_mcaid_dis), color = "blue", size = 1) +
+  geom_line(aes(y = totaltax * scale3), color = "red", size = 1) +
   scale_y_continuous(
     name = "Average Medicaid Discharges Proportion",
-    sec.axis = sec_axis(~ . / scale3, name = "Cumulative Provider Taxes")
+    sec.axis = sec_axis(~ . / scale3, name = "Number of States Adopted Tax")
   ) +
-  labs(
-    x = "Year",
-    title = "Average Medicaid Discharges Proportion and Cumulative Provider Taxes Over Time"
-  ) +
-  theme_minimal()
+  theme_minimal() +
+  theme(
+    axis.title.y.left = element_text(color = "blue"),
+    axis.title.y.right = element_text(color = "red")
+  )
 
 ggsave("sumplots/mcaiddispropplot.png", plot = mcaiddispropplot, width = 8, height = 8, dpi = 300)
 
+# dual axis: uncompensated care charrges and total states with tax
+plot4 <- hospdata %>%
+  filter(!is.na(tot_uncomp_care_charges) & year < 2025 & year > 2003) %>%
+  group_by(year) %>%
+  summarise(
+    avg_uc_charges = mean(tot_uncomp_care_charges, na.rm = TRUE),
+    totaltax = first(totaltax[!is.na(totaltax)]),
+    .groups = "drop"
+  )
+
+scale4 <- max(plot4$avg_uc_charges, na.rm = TRUE) / max(plot4$totaltax, na.rm = TRUE)
+
+uncompplot <- ggplot(plot4, aes(x = year)) +
+  geom_line(aes(y = avg_uc_charges), color = "blue", size = 1) +
+  geom_line(aes(y = totaltax * scale4), color = "red", size = 1) +
+  scale_y_continuous(
+    name = "Average Uncompensated Care Charges",
+    sec.axis = sec_axis(~ . / scale4, name = "Number of States Adopted Tax")
+  ) +
+  theme_minimal() +
+  theme(
+    axis.title.y.left = element_text(color = "blue"),
+    axis.title.y.right = element_text(color = "red")
+  )
+
+ggsave("sumplots/uncompplot.png", plot = uncompplot, width = 8, height = 8, dpi = 300)
+
+# dual axis: cost to charge ratio and total states with tax
+plot5 <- hospdata %>%
+  filter(!is.na(cost_to_charge) & cost_to_charge < 10 & year < 2025 & year > 2003) %>%
+  group_by(year) %>%
+  summarise(
+    avg_ccr = mean(cost_to_charge, na.rm = TRUE),
+    totaltax = first(totaltax[!is.na(totaltax)]),
+    .groups = "drop"
+  )
+
+scale5 <- max(plot5$avg_ccr, na.rm = TRUE) / max(plot5$totaltax, na.rm = TRUE)
+
+ccrplot <- ggplot(plot5, aes(x = year)) +
+  geom_line(aes(y = avg_ccr), color = "blue", size = 1) +
+  geom_line(aes(y = totaltax * scale5), color = "red", size = 1) +
+  scale_y_continuous(
+    name = "Average Cost to Charge Ratio",
+    sec.axis = sec_axis(~ . / scale5, name = "Number of States Adopted Tax")
+  ) +
+  theme_minimal() +
+  theme(
+    axis.title.y.left = element_text(color = "blue"),
+    axis.title.y.right = element_text(color = "red")
+  )
+ggsave("sumplots/ccrplot.png", plot = ccrplot, width = 8, height = 8, dpi = 300)
